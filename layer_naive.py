b040b5bb48438b43c78b0c5f0bde83f58395774a
harukaeru <nekoneko.myaomyao@gmail.com>
2026-06-25 20:13:54 +0900

    Sigmoid Layer

--- layer_naive.py
+++ layer_naive.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MulLayer:
   def __init__(self):
     self.x = None
@@ -25,35 +27,74 @@ class AddLayer:
     return dx, dy
 
 
+class Relu:
+  def __init__(self):
+    self.mask = None
 
-apple = 100
-apple_num = 2
-tax = 1.1
+  def forward(self, x):
+    self.mask = (x <= 0)
+    out = x.copy()
+    out[self.mask] = 0
+    return out
 
-orange = 150
-orange_num = 3
+  def backward(self, dout):
+    dout[self.mask] = 0
+    dx = dout
+    return dx
 
-mul_apple_layer = MulLayer()
-mul_orange_layer = MulLayer()
-aggregate_layer = AddLayer()
-mul_tax_layer = MulLayer()
+class Sigmoid:
+  def __init__(self):
+    self.out = None
+
+  def forward(self, x):
+    out = 1 / (1 + np.exp(-x))
+    self.out = out
+    return out
+  
+  def backward(self, dout):
+    dx = dout * (1.0 - self.out) * self.out
+    return dx
 
 
-apple_price = mul_apple_layer.forward(apple, apple_num)
-orange_price = mul_orange_layer.forward(orange, orange_num)
-subtotal = aggregate_layer.forward(apple_price, orange_price)
-price = mul_tax_layer.forward(subtotal, tax)
+x = np.array([
+  [1.0, -0.5],
+  [-2.0, 3.0]
+])
 
-print(price)
+print(x)
 
-# backward
-dprice = 1
-dsubtotal, dtax = mul_tax_layer.backward(dprice)
-dapple_price, dorange_price = aggregate_layer.backward(dsubtotal)
-dapple, dapple_num = mul_apple_layer.backward(dapple_price)
-dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+relu = Relu()
+print('out:')
+print(relu.forward(x))
 
-print('apple:', dapple, dapple_num)
-print('orange:', dorange, dorange_num)
-print('tax:', dtax)
-print('subtotal:', dsubtotal)
+# apple = 100
+# apple_num = 2
+# tax = 1.1
+# 
+# orange = 150
+# orange_num = 3
+# 
+# mul_apple_layer = MulLayer()
+# mul_orange_layer = MulLayer()
+# aggregate_layer = AddLayer()
+# mul_tax_layer = MulLayer()
+# 
+# 
+# apple_price = mul_apple_layer.forward(apple, apple_num)
+# orange_price = mul_orange_layer.forward(orange, orange_num)
+# subtotal = aggregate_layer.forward(apple_price, orange_price)
+# price = mul_tax_layer.forward(subtotal, tax)
+# 
+# print(price)
+# 
+# # backward
+# dprice = 1
+# dsubtotal, dtax = mul_tax_layer.backward(dprice)
+# dapple_price, dorange_price = aggregate_layer.backward(dsubtotal)
+# dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+# dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+# 
+# print('apple:', dapple, dapple_num)
+# print('orange:', dorange, dorange_num)
+# print('tax:', dtax)
+# print('subtotal:', dsubtotal)
