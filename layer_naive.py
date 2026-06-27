5cf941c83b11de32ee8d8c3aee642b786f7863c2
harukaeru <nekoneko.myaomyao@gmail.com>
2026-06-27 16:14:53 +0900

    2026/06/27

--- layer_naive.py
+++ layer_naive.py
@@ -1,4 +1,5 @@
 import numpy as np
+from common.functions import softmax, cross_entropy_error
 
 class MulLayer:
   def __init__(self):
@@ -54,6 +55,45 @@ class Sigmoid:
   def backward(self, dout):
     dx = dout * (1.0 - self.out) * self.out
     return dx
+
+
+class Affine:
+  def __init__(self, W, b):
+    self.W = W
+    self.b = b
+    self.x = None
+    self.dW = None
+    self.db = None
+
+  def forward(self, x):
+    self.x = x
+    out = np.dot(x, self.W) + b
+    return out
+
+  def backward(self, dout):
+    dx = np.dot(dout, self.W.T)
+    self.dW = np.dot(self.x.T, dout)
+    self.db = np.dot(dout, axis=0)
+    return dx
+
+
+class SoftmaxWithLoss:
+  def __init__(self):
+    self.loss = None
+    self.y = None
+    self.t = None
+  
+  def forward(self, x, t):
+    self.t = t
+    self.y = softmax(x)
+    self.loss = cross_entropy_error(self.y, self.t)
+    return self.loss
+  
+  def backward(self, dout=1):
+    batch_size = self.t.shape[0]
+    dx = (self.y - self.t) / batch_size
+    return dx
+
 
 
 x = np.array([
